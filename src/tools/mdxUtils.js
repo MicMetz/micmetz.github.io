c9553a4cfd7cbc391c53a9ca010da4865fb342bd
MicMetz <mime9599@colorado.edu>
2023-06-07 10:38:01 -0700

    setting mdx

--- src/tools/mdxUtils.js
+++ src/tools/mdxUtils.js
@@ -4,30 +4,30 @@ const path = require( "path" );
 
 
 // ESSAYS_PATH is useful when you want to get the path to a specific file
-const ESSAYS_PATH = path.join(process.cwd(), "posts", "essays");
+const ESSAYS_PATH = path.join( process.cwd(), "posts", "essays" );
 
 const essayFilePaths = fs
-.readdirSync(ESSAYS_PATH)
+.readdirSync( ESSAYS_PATH )
 // Only include MDX files
-.filter((path) => /\.mdx?$/.test(path));
+.filter( ( path ) => /\.mdx?$/.test( path ) );
 
-const NOTES_PATH = path.join(process.cwd(), "posts", "notes");
+const NOTES_PATH = path.join( process.cwd(), "posts", "notes" );
 
 const noteFilePaths = fs
-.readdirSync(NOTES_PATH)
-.filter((path) => /\.mdx?$/.test(path));
+.readdirSync( NOTES_PATH )
+.filter( ( path ) => /\.mdx?$/.test( path ) );
 
-const PATTERNS_PATH = path.join(process.cwd(), "posts", "patterns");
+const PATTERNS_PATH = path.join( process.cwd(), "posts", "patterns" );
 
 const patternFilePaths = fs
-.readdirSync(PATTERNS_PATH)
-.filter((path) => /\.mdx?$/.test(path));
+.readdirSync( PATTERNS_PATH )
+.filter( ( path ) => /\.mdx?$/.test( path ) );
 
-const PROJECTS_PATH = path.join(process.cwd(), "posts", "projects");
+const PROJECTS_PATH = path.join( process.cwd(), "posts", "projects" );
 
 const projectFilePaths = fs
-.readdirSync(PROJECTS_PATH)
-.filter((path) => /\.mdx?$/.test(path));
+.readdirSync( PROJECTS_PATH )
+.filter( ( path ) => /\.mdx?$/.test( path ) );
 
 module.exports = {
 		essayFilePaths,
