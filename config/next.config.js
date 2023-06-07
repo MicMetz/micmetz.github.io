f4dd613275074c4491eaa78a97fc373e8a020d1a
MicMetz <mime9599@colorado.edu>
2023-06-07 10:48:40 -0700

    setting mdx

--- config/next.config.js
+++ config/next.config.js
@@ -38,13 +38,15 @@ module.exports = withPWA( {
 						},
 				},
 		],
-		webpack(config) { // we depend on nextjs switching to webpack 4 by default. Probably they will
-				// change this behavior at some future major version.
-				config.node = {
-						fs: "empty", // webpack4 era solution
-				};
+		webpack(config, { nextRuntime }) {
+						// as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
+						if (typeof nextRuntime === "undefined") {
+								const { IgnorePlugin } = require("webpack");
+								const ignoreFs = new IgnorePlugin({ resourceRegExp: /fs/ });
+								config.plugins.push(ignoreFs);
+						}
 
-				return config;
+						return config;
 		},
 		async redirects() {
 				return [
--- src/tools/mdxUtils.js
+++ src/tools/mdxUtils.js
@@ -1,32 +1,29 @@
-const fs = require( "fs" );
+// const fs = require( "fs" );
 const path = require( "path" );
+const { readdirSync } = require( "fs" );
 
 
 
 // ESSAYS_PATH is useful when you want to get the path to a specific file
 const ESSAYS_PATH = path.join( process.cwd(), "posts", "essays" );
 
-const essayFilePaths = fs
-.readdirSync( ESSAYS_PATH )
+const essayFilePaths = readdirSync( ESSAYS_PATH )
 // Only include MDX files
 .filter( ( path ) => /\.mdx?$/.test( path ) );
 
 const NOTES_PATH = path.join( process.cwd(), "posts", "notes" );
 
-const noteFilePaths = fs
-.readdirSync( NOTES_PATH )
+const noteFilePaths = readdirSync( NOTES_PATH )
 .filter( ( path ) => /\.mdx?$/.test( path ) );
 
 const PATTERNS_PATH = path.join( process.cwd(), "posts", "patterns" );
 
-const patternFilePaths = fs
-.readdirSync( PATTERNS_PATH )
+const patternFilePaths = readdirSync( PATTERNS_PATH )
 .filter( ( path ) => /\.mdx?$/.test( path ) );
 
 const PROJECTS_PATH = path.join( process.cwd(), "posts", "projects" );
 
-const projectFilePaths = fs
-.readdirSync( PROJECTS_PATH )
+const projectFilePaths = readdirSync( PROJECTS_PATH )
 .filter( ( path ) => /\.mdx?$/.test( path ) );
 
 module.exports = {
