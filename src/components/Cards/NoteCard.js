0e72ab62a8bb3a37cbed10ed8fd0b1a6e19ed751
MicMetz <mime9599@colorado.edu>
2023-06-07 19:04:21 -0700

    setting mdx

--- src/components/Cards/NoteCard.js
+++ src/components/Cards/NoteCard.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import GrowthIcon from "../../components/Icons/GrowthIcon.js";
 import { NoteMetadataContainer, StyledNoteCard } from "../../styles/StyledCardComponents/NoteStyled.js";
 import Link from "next/link";
@@ -5,25 +6,46 @@ import { RelativeDate } from "../MISC/DatesFormat.js";
 
 
 
-export default function NoteCard( { slug, title, growthStage, date, id } ) {
+
+export default function NoteCard({ slug, title, growthStage, date, id }) {
 		return (
-				<Link key = {id} as = {`/${slug}`} href = {`/${slug}`} >
-						<a >
-								<StyledNoteCard >
-										{growthStage && <GrowthIcon size = "24" growthStage = {growthStage} />}
-										<div >
-												<h3 >{title}</h3 >
-												<NoteMetadataContainer >
-														<span >{growthStage} Note</span >
-														<svg width = "6px" height = "14px" >
-																<circle r = "3" cx = "3" cy = "3" fill = "var(--color-gray-400)" />
-														</svg >
-														<RelativeDate postDate = {date} />
-												</NoteMetadataContainer >
-										</div >
-								</StyledNoteCard >
-						</a >
-				</Link >
+				<Link key={id} as={`/${slug}`} href={`/${slug}`}>
+						<a>
+								<StyledNoteCard>
+										{growthStage && <GrowthIcon size="24" growthStage={growthStage} />}
+										<div>
+												<h3>{title}</h3>
+												<MetadataContainer>
+														<span>{growthStage} Note</span>
+														<svg width="6px" height="14px">
+																<circle r="3" cx="3" cy="3" fill="var(--color-gray-400)" />
+														</svg>
+														<RelativeDate postDate={date} />
+												</MetadataContainer>
+										</div>
+								</StyledNoteCard>
+						</a>
+				</Link>
 		);
 }
 
+
+
+
+const MetadataContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-left: var(--space-xs);
+  margin-top: var(--space-2xs);
+  font-family: var(--font-sans);
+  font-size: calc(var(--font-size-xs) / 1.08);
+  color: var(--color-gray-500);
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  font-weight: 400;
+  svg {
+    margin: 0 0.4rem;
+  }
+`;
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,14 +1,14 @@
 // import {noteFilePaths,  NOTES_PATH } from "..//tools/mdxUtils.js";
-import {noteFilePaths,  NOTES_PATH } from "../tools/mdxUtils.js";
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import React from 'react';
 import NoteCard from "../components/Cards/NoteCard.js";
 import Header from "../components/Layouts/Header.js";
 import { Layout } from "../components/Layouts/Layout.js";
 import MasonryGrid from "../components/Layouts/MasonryGrid.js";
 import TitleWithCount from "../components/MISC/TitleWithCount.js";
-import fs from "fs";
-import matter from "gray-matter";
-import path from "path";
-import React from 'react';
+import { noteFilePaths, NOTES_PATH } from "../tools/mdxUtils.js";
 
 
 
@@ -23,13 +23,13 @@ export default function NotesPage( { notes } ) {
 								<MasonryGrid >
 										{notes.map( ( essay, i ) => (
 												<NoteCard
-														id={note.slug}
-														slug={note.slug}
-														title={note.data.title}
-														growthStage={note.data.growthStage}
-														date={note.data.updated}
+														id = {note.slug}
+														slug = {note.slug}
+														title = {note.data.title}
+														growthStage = {note.data.growthStage}
+														date = {note.data.updated}
 												/>
-										))}
+										) )}
 								</MasonryGrid >
 						</Layout >
 				</>
@@ -39,10 +39,10 @@ export default function NotesPage( { notes } ) {
 
 export function getStaticProps() {
 		// Get all note posts
-		let notes = noteFilePaths.map((filePath) => {
-				const source = fs.readFileSync(path.join(NOTES_PATH, filePath));
-				const { content, data } = matter(source);
-				const slug = filePath.replace(/\.mdx$/, "");
+		let notes = noteFilePaths.map( ( filePath ) => {
+				const source = fs.readFileSync( path.join( NOTES_PATH, filePath ) );
+				const { content, data } = matter( source );
+				const slug = filePath.replace( /\.mdx$/, "" );
 
 				return {
 						content,
@@ -50,12 +50,12 @@ export function getStaticProps() {
 						slug,
 						filePath,
 				};
-		});
+		} );
 
 		// Sort notes by date
-		const sortedNotes = notes.sort((a, b) => {
-				return new Date(b.data.updated) - new Date(a.data.updated);
-		});
+		const sortedNotes = notes.sort( ( a, b ) => {
+				return new Date( b.data.updated ) - new Date( a.data.updated );
+		} );
 		notes = sortedNotes;
 
 		return { props: { notes } };
